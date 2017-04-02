677e3312b241bbe4d2d7d62ad89923cc0c2535e2
medium007 <patryk.milewczyk95@gmail.com>
2017-04-02 21:10:52 +0200

    Refactoring

---
+++ JoyStick/src/console.py
@@ -0,0 +1,31 @@
+import pygame as pg
+import time
+
+pg.init()
+pg.joystick.init()
+
+js = pg.joystick.Joystick(0)
+js.init()
+print(js.get_name())
+
+joystick_count = pg.joystick.get_count()
+
+print("Number of joysticks: {}".format(joystick_count))
+
+buttons = js.get_numbuttons()
+print("Number of buttons: {}".format(buttons))
+
+time.sleep(5)
+
+
+while(1):
+    for event in pg.event.get():
+        axis = js.get_numaxes()
+        for i in range(buttons):
+            button = js.get_button(i)
+            print("Button {:>2} value: {}".format(i, button))
+        for i in range(axis):
+            ax = js.get_axis(i)
+            print("Axis {} value: {:>6.3f}".format(i, ax))
+        print("\n")
+
