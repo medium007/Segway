bbfdbc46fa800a93089b5c92a772c679772eb402
gree246 <CpU^7uIE>
2017-06-07 23:40:09 +0200

    project refactoring

--- segwaysim/main.py
+++ segwaysim/main.py
@@ -11,19 +11,22 @@ process.wait()
 for line in process.stdout:
         print(line)
 
-cmd=['morse','run','segwaysim']                                                 # running morse project
-cmd2=['python3',dir_path+'/scripts/rownowaga.py']                               # running script which controls segaway
-cmd3=['python3',dir_path+'/scripts/chase_controller.py']
+cmd=['morse','run','segwaysim']                                                              # running morse project
 process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
 
+
+cmd2=['python3',dir_path+'/scripts/main_segway_controller.py']                               # running script which controls main segaway
 process2 = subprocess.Popen(cmd2, stdout=subprocess.PIPE)
 
+cmd3=['python3',dir_path+'/scripts/chase_controller.py']                                     # running script which controls chase segaway
 process3 = subprocess.Popen(cmd3, stdout=subprocess.PIPE)
 
 
-process.wait()
-process2.kill()   # exiting rownowaga.py when you close blender
+process.wait()    # wait for main window close
+process2.kill()   # exiting main_segway_controller.py when you close blender
 process3.kill()   # exiting ruch_chase.py when you close blender
 
+for line in process.stdout and process2.stdout and process3.stdout:
+        print(line)
 
 
